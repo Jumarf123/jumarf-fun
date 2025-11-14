aeb115c82ebcf7225ddb31f89df1d31a4ba420cd
Jumarf123 <simonenkob819@gmail.com>
2025-11-14 23:24:20 +0800

    Update build configuration to output to root directory

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
     outDir: '.',
-    assetsDir: 'assets'
+    emptyOutDir: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]'
+      }
+    }
+  },
+  server: {
+    port: 3000
   }
 })
